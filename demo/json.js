8dcf85b527aea954f2f93d285a34db9b6cb8c260
Stuart Loxton <stuart@strawberrysoup.co.uk>
2008-10-10 17:05:45 +0100

    Added extra tooltip in JSON page to show off feature.

--- demo/json.js
+++ demo/json.js
@@ -1,5 +1,6 @@
 {
 	examples: {
-		json: '<p>Using JSON you can go across domains easily and hold several tooltips in one remote page.'
+		json: '<p>Using JSON you can go across domains easily and hold several tooltips in one remote page.',
+		secondTip: '<p>This is to show that you can have multiple tips in one JSON file.</p>'
 	}
 }
